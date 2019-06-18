5b42bc4fd4dbf49e9badc92b67ffa08b88cda8aa
Gunsum117 <juniwamoto1117@gmail.com>
2019-06-18 05:34:48 +0000

    admin購入履歴一覧/注文ステータス変更機能実装

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,14 +1,25 @@
 class Admin::OrdersController < ApplicationController
 
-  before_action :authenticate_admin!, only: [:index, :show, :update]
+  before_action :authenticate_admin!, only: [:index, :show, :update, :show]
 
   def index
+  	@orders = Order.all
   end
 
   def show
+  	@order = Order.find(params[:id])
   end
 
   def update
+  	order = Order.find(params[:id])
+  	order.update(order_params)
+  	redirect_to admin_order_path(order)
+  end
+
+private
+
+  def order_params
+  params.require(:order).permit(:order_status)
   end
 
 end
