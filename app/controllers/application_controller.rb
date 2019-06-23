1fba97fa2850c1712be18026097dc49bdf1a57db
hiko3 <maymay1353@gmail.com>
2019-06-23 09:31:10 +0000

    adminのログイン後のパス修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,19 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
+    if current_user
   		root_path
+    elsif current_admin
+       admin_items_path
+    end
   end
 
   def after_sign_up_path_for(resource)
-  		root_path
+  		if current_user
+      root_path
+    elsif current_admin
+       admin_items_path
+    end
   end
 
   def after_sign_out_path_for(resource)
